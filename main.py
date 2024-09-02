089ad9fb0750c3390e01fce5c93c4280de3fdf00
Lingxi <2863878907@qq.com>
2024-09-02 15:53:33 +0800

    Comment of fechter params and course

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ if __name__ == '__main__':
     login_config = LoginConfig(test_config.get_username(), test_config.get_password())
     session_id = AccountValidation(login_config).get_login_session_id()
 
+    """ 样例
+    xnxq="2024-1" 代表2024年第二个学期
+    kcfw="Specialty" 代表专业课
+    sel_nj="2023" 代表选课的年级 为2023级
+    sel_zydm="0150" 代表选择的专业 为0150智能科学
+    sel_schoolarea="3" 代表选择的校区 为西海岸校区
+    """
     fetcher_params = FetcherParams(
-        initQry=0, xktype=2, xh="", xn="2024", xq=0, nj="2022", zydm="0143",
-        items="", xnxq="2024-0", kcfw="Specialty", sel_nj="2022", sel_zydm="0143",
-        sel_schoolarea=""
+        xnxq="2024-1", kcfw="Specialty", sel_nj="2023", sel_zydm="0150", sel_schoolarea="3"
     ).to_dict()
 
     fetcher_config = FetcherConfig(session_id=session_id, params=fetcher_params)
